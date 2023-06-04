dd787f78302e5e8361d7f7655a2bd4e0950960ea
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2023-06-04 01:27:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
-
-/* 
-const express = require('express');
-const { Pool } = require('pg');
-
-const app = express();
-
-app.use(express.json());
-
-const pool = new Pool({
-    user: 'your_username',
-    host: 'your_host',
-    database: 'your_database',
-    password: 'your_password',
-    port: 5432
-  });
-
-  const router = express.Router();
-
-// ruta
-*/
-
-
-
-/*
-router.get('/joyas', async (req, res) => {
-  try {
-    
-    const cliente = await pool.connect();
-    const result = await cliente.query('SELECT * FROM joyas');
-    res.json(result.rows);
-    cliente.release(); 
-    
-   res.json({ok:true})
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Error de server interno' });
-  }
-});
-
-app.use('/api', router);
-*/
-
 require('dotenv').config();
 
 const express = require("express");
+//import pool from "./database/connection.js";
+const pool = require("./database/connection.js").default;
+
 
 const app = express()
 
@@ -51,7 +11,15 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Servidor corriendo en puerto 3000"));
 
 
-app.get("/joyas", (req, res) => {
-    //res.send("Hola mundo joyas");
-    res.json({ ok: true });
-})
+app.get("/joyas", async (req, res) => {
+    try {
+        const query = "SELECT * FROM inventario";
+        const response = await pool.query(query);
+        res.json(response)
+        //res.json({ ok: true });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error de server interno' });
+    }
+    
+});
