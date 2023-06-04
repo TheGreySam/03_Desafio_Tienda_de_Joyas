5990542db5effc7d736735637d0c40daa8580d45
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2023-06-04 01:38:27 -0400

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,21 +1,7 @@
-//import * as dotenv from "dotenv";
 require('dotenv').config();
-
-//dotenv.config();
-
-
-//import pkg from "pg";
-//const { Pool } = require("pg");
-//const pkg = { Pool };
 
 const { Pool } = require("pg");
 const pool = new Pool({ allowExitOnIdle: true });
 module.exports = { pool };
 
-
-//const { Pool } = pkg;
-
 //console.log(process.env.PGPASSWORD);
-
-//export const pool = new Pool({allowExitOnIdle: true});
-//export default pool;
