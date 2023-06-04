ad8ac577d1c213292a11cec1a8b8b960d70aae1a
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2023-06-04 02:24:17 -0400

    actualizacion

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require("express");
 //import pool from "./database/connection.js";
 const pool = require("./database/connection.js").default;
-
+console.log(pool)
 
 const app = express()
 
@@ -13,7 +13,7 @@ app.listen(PORT, () => console.log("Servidor corriendo en puerto 3000"));
 
 app.get("/joyas", async (req, res) => {
     try {
-        const query = "SELECT * FROM inventario";
+        const query = 'SELECT * FROM inventario';
         const response = await pool.query(query);
         res.json(response)
         //res.json({ ok: true });
